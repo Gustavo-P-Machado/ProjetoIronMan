aaca4a4a1e03c2993dd9052fa320147fb8c83e37
Gustavo.P.Machado <pangustavo34@gmail.com>
2024-06-04 21:53:17 -0300

    Versão com várias telas e gráficos diferentes

--- main.py
+++ main.py
@@ -3,114 +3,191 @@ pygame.init() #inicia a biblioteca "pygame"
 tamanho = (1080, 720) #tamanho da tela
 clock = pygame.time.Clock() #define o fps 
 tela = pygame.display.set_mode(tamanho) #cria o display da tela
-pygame.display.set_caption('IronMan GG') #cria o título da tela
+pygame.display.set_caption('Jogo Do Amongus') #cria o título da tela
 
 fonte = pygame.font.SysFont('comicsans', 20)
 fonteMorte = pygame.font.SysFont("arial",120)
-pygame.mixer.music.load('assets/ironsound.mp3')
-pygame.mixer.music.play(-1)
+fonteStart = pygame.font.SysFont("comicsans",55)
 
-missileSound = pygame.mixer.Sound('assets/missile.wav')
-pygame.mixer.Sound.play(missileSound)
+imgicone = pygame.image.load('assets/icon.png')
+icone = pygame.transform.scale(imgicone, (32, 32))
+pygame.display.set_icon(icone)
 
-explosaoSound = pygame.mixer.Sound("assets/explosao.wav")
+imgStart = pygame.image.load("assets/fundoStart.jpg")
+fundoStart = pygame.transform.scale(imgStart, (1080, 720))
+imgDead = pygame.image.load("assets/fundoDead.jpg")
+fundoDead = pygame.transform.scale(imgDead, (1080, 720))
+
+
+morteSound = pygame.mixer.Sound("assets/deadSound.mp3")
+
 
 branco = (255, 255, 255)
 preto = (0, 0, 0)
+verdeEscuro = (0, 26, 39)
 
-ironman = pygame.image.load('assets/iron.png')
-picture = pygame.image.load('assets/fundo.png')
-bg = pygame.transform.scale(picture, (1080, 720))
-missil = pygame.image.load('assets/missile.png')
-icone  = pygame.image.load("assets/icone.png")
-pygame.display.set_icon(icone)
+def jogar():
 
-tela.set_alpha
+    pygame.mixer.music.stop()
 
-posXPers = 400
-posYPers = 300
-movimentoXPers = 0
-movimentoYPers = 0
+    pygame.mixer.music.load('assets/jogoSound.mp3')
+    pygame.mixer.music.play(-1)
 
-posXmissile = 400
-posYmissile = -240
-velmissile = 10
+    facaSound = pygame.mixer.Sound('assets/facaSound.mp3')
+    pygame.mixer.Sound.play(facaSound)
 
-pontos = 0
-larguraPersona = 250
-alturaPersona = 127
-larguaMissel  = 50
-alturaMissel  = 250
-dificuldade  = 0
+    imgamogus = pygame.image.load('assets/amogus.webp')
+    amogus = pygame.transform.scale(imgamogus, (100, 127))
 
-while True: #mantém a tela aberta até que a tela seja fechada
-    for evento in pygame.event.get():
-        if evento.type == pygame.QUIT:
-            quit()
-        elif evento.type == pygame.KEYDOWN and evento.key == pygame.K_RIGHT:
-            movimentoXPers = 20
-        elif evento.type == pygame.KEYDOWN and evento.key == pygame.K_LEFT:
-            movimentoXPers = -20
-        elif evento.type == pygame.KEYUP and evento.key == pygame.K_RIGHT:
-            movimentoXPers = 0
-        elif evento.type == pygame.KEYUP and evento.key == pygame.K_LEFT:
-            movimentoXPers = 0
+    picture = pygame.image.load('assets/fundo.png')
+    bg = pygame.transform.scale(picture, (1080, 720))
 
-        elif evento.type == pygame.KEYDOWN and evento.key == pygame.K_UP:
-            movimentoYPers = -20
-        elif evento.type == pygame.KEYDOWN and evento.key == pygame.K_DOWN:
-            movimentoYPers = 20
-        elif evento.type == pygame.KEYUP and evento.key == pygame.K_UP:
-            movimentoYPers = 0
-        elif evento.type == pygame.KEYUP and evento.key == pygame.K_DOWN:
-            movimentoYPers = 0
-    
-    posXPers = posXPers + movimentoXPers
-    if posXPers < 0:
-        posXPers = 10
-    elif posXPers > 830: 
-        posXPers = 820
+    imgfaca = pygame.image.load('assets/faca.png')
+    faca = pygame.transform.scale(imgfaca, (75, 150))
 
-    posYPers = posYPers + movimentoYPers
-    if posYPers < 0:
-        posYPers = 10
-    elif posYPers > 593:
-        posYPers = 583
 
-    tela.fill(branco) #diz qual é a cor de background
-    tela.blit(bg, (0, 0))
-    tela.blit(ironman, (posXPers, posYPers))
-    
-    posYmissile = posYmissile + velmissile
-    if posYmissile > 720:
-        posYmissile = -240
-        pontos = pontos + 1
-        velmissile = velmissile + 1
-        posXmissile = random.randint(0, 1080)
-        pygame.mixer.Sound.play(missileSound)
 
-    tela.blit(missil, (posXmissile, posYmissile))
+    tela.set_alpha
 
-    pixelsPersonaX = list(range(posXPers, posXPers+larguraPersona))
-    pixelsPersonaY = list(range(posYPers, posYPers+alturaPersona))
-    pixelsMisselX = list(range(posXmissile, posXmissile + larguaMissel))
-    pixelsMisselY = list(range(posYmissile, posYmissile + alturaMissel))
-    
+    posXPers = 540
+    posYPers = 460
+    movimentoXPers = 0
+    movimentoYPers = 0
 
-    if  len( list( set(pixelsMisselY).intersection(set(pixelsPersonaY))) ) > dificuldade:
-        if len( list( set(pixelsMisselX).intersection(set(pixelsPersonaX))   ) )  > dificuldade:
+    posXmissile = 400
+    posYmissile = -150
+    velmissile = 10
 
-            textoMorte = fonteMorte.render("Morreuuuu", True, preto)
-            tela.blit(textoMorte, (200,300))
-            pygame.mixer.Sound.play(explosaoSound)
+    pontos = 0
+    larguraPersona = 100
+    alturaPersona = 127
+    larguaMissel  = 50
+    alturaMissel  = 100
+    dificuldade  = 0
 
-    #pygame.draw.circle(tela, preto, (posXPers, posYPers), 40, 0)
-    #texto = fonte.render(str(posXPers) + '-' + str(posYPers), True, branco)
-    txtpnts = fonte.render('Pontos: ' + str(pontos), True, branco)
-    tela.blit(txtpnts, (20, 20))
-    #tela.blit(texto, (posXPers-30, posYPers-10))
+    while True: #mantém a tela aberta até que a tela seja fechada
+        for evento in pygame.event.get():
+            if evento.type == pygame.QUIT:
+                quit()
+            elif evento.type == pygame.KEYDOWN and evento.key == pygame.K_ESCAPE:
+                quit()
+            elif evento.type == pygame.KEYDOWN and evento.key == pygame.K_RIGHT:
+                movimentoXPers = 20
+            elif evento.type == pygame.KEYDOWN and evento.key == pygame.K_LEFT:
+                movimentoXPers = -20
+            elif evento.type == pygame.KEYUP and evento.key == pygame.K_RIGHT:
+                movimentoXPers = 0
+            elif evento.type == pygame.KEYUP and evento.key == pygame.K_LEFT:
+                movimentoXPers = 0
 
-    pygame.display.update() #diz pra atualizar a tela
-    clock.tick(60) #diz que a tela vai ter 60 fps
+            elif evento.type == pygame.KEYDOWN and evento.key == pygame.K_UP:
+                movimentoYPers = -20
+            elif evento.type == pygame.KEYDOWN and evento.key == pygame.K_DOWN:
+                movimentoYPers = 20
+            elif evento.type == pygame.KEYUP and evento.key == pygame.K_UP:
+                movimentoYPers = 0
+            elif evento.type == pygame.KEYUP and evento.key == pygame.K_DOWN:
+                movimentoYPers = 0
+        
+        posXPers = posXPers + movimentoXPers
+        if posXPers < 0:
+            posXPers = 10
+        elif posXPers > 980: 
+            posXPers = 970
 
-pygame.quit()
+        posYPers = posYPers + movimentoYPers
+        if posYPers < 0:
+            posYPers = 10
+        elif posYPers > 593:
+            posYPers = 583
+
+        tela.fill(branco) #diz qual é a cor de background
+        tela.blit(bg, (0, 0))
+        tela.blit(amogus, (posXPers, posYPers))
+        
+        posYmissile = posYmissile + velmissile
+        if posYmissile > 720:
+            posYmissile = -150
+            pontos = pontos + 1
+            velmissile = velmissile + 1
+            posXmissile = random.randint(0, 1080)
+            pygame.mixer.Sound.play(facaSound)
+
+        tela.blit(faca, (posXmissile, posYmissile))
+
+        pixelsPersonaX = list(range(posXPers, posXPers+larguraPersona))
+        pixelsPersonaY = list(range(posYPers, posYPers+alturaPersona))
+        pixelsMisselX = list(range(posXmissile, posXmissile + larguaMissel))
+        pixelsMisselY = list(range(posYmissile, posYmissile + alturaMissel))
+        
+
+        if  len( list( set(pixelsMisselY).intersection(set(pixelsPersonaY))) ) > dificuldade:
+            if len( list( set(pixelsMisselX).intersection(set(pixelsPersonaX))   ) )  > dificuldade:
+                dead()
+
+
+        #pygame.draw.circle(tela, preto, (posXPers, posYPers), 40, 0)
+        #texto = fonte.render(str(posXPers) + '-' + str(posYPers), True, branco)
+        txtpnts = fonte.render('Pontos: ' + str(pontos), True, branco)
+        tela.blit(txtpnts, (20, 20))
+        #tela.blit(texto, (posXPers-30, posYPers-10))
+
+        pygame.display.update() #diz pra atualizar a tela
+        clock.tick(60) #diz que a tela vai ter 60 fps
+
+def dead():
+    pygame.mixer.music.stop()
+    pygame.mixer.Sound.play(morteSound)
+    pygame.mixer.music.load('assets/musicaDead.mp3')
+    pygame.mixer.music.play(-1)
+
+    while True: #mantém a tela aberta até que a tela seja fechada
+        for evento in pygame.event.get():
+            if evento.type == pygame.QUIT:
+                quit()
+            elif evento.type == pygame.KEYDOWN and evento.key == pygame.K_ESCAPE:
+                quit()
+            elif evento.type == pygame.MOUSEBUTTONDOWN:
+                if buttonStart.collidepoint(evento.pos):
+                    jogar()
+            elif evento.type == pygame.KEYDOWN and evento.key == pygame.K_KP_ENTER:
+                jogar()
+                
+        tela.fill(branco)
+        tela.blit(fundoDead, (0,0))
+        buttonStart = pygame.draw.rect(tela, verdeEscuro, (35,495,750,100), 1)
+        textoStart = fonteStart.render("RESTART", True, branco)
+        tela.blit(textoStart, (400,510))
+        textoEnter = fonte.render("Press enter to continue...", True, branco)
+        tela.blit(textoEnter, (60,530))
+      
+        
+        pygame.display.update()
+        clock.tick(60)
+
+def start():
+    pygame.mixer.music.load('assets/musicaStart.mp3')
+    pygame.mixer.music.play(-1)
+    while True: #mantém a tela aberta até que a tela seja fechada
+        for evento in pygame.event.get():
+            if evento.type == pygame.QUIT:
+                quit()
+            elif evento.type == pygame.KEYDOWN and evento.key == pygame.K_ESCAPE:
+                quit()
+            elif evento.type == pygame.MOUSEBUTTONDOWN:
+                if buttonStart.collidepoint(evento.pos):
+                    jogar()
+            elif evento.type == pygame.KEYDOWN and evento.key == pygame.K_KP_ENTER:
+                jogar()
+
+
+
+        tela.fill(branco)
+        tela.blit(fundoStart, (0,0))
+        buttonStart = pygame.draw.rect(tela, preto, (110,482,600,100),0)
+        textoStart = fonteStart.render("START", True, branco)
+        tela.blit(textoStart, (330,482))
+        pygame.display.update()
+        clock.tick(60)
+
+start()  
